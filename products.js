f9103c86bbb691089dcf83d57a3ef852aed26003
Busulka <83404095+Busulka@users.noreply.github.com>
2021-07-03 01:43:19 +0300

    homework8

---
+++ products.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const products = [
+    new ProductDTO(
+        0,
+        '0.jpg',
+        'Product 0',
+        'Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        52.00,
+    ),
+    new ProductDTO(
+        1,
+        '1.jpg',
+        'Product 1',
+        'Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        72.00,
+    ),
+    new ProductDTO(
+        2,
+        '2.jpg',
+        'Product 2',
+        'Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        82.00,
+    ),
+    new ProductDTO(
+        3,
+        '3.jpg',
+        'Product 3',
+        'Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        82.99,
+    ),
+    new ProductDTO(
+        4,
+        '4.jpg',
+        'Product 4',
+        'Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        92.00,
+    ),
+    new ProductDTO(
+        5,
+        '5.jpg',
+        'Product 5',
+        'Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        79.55,
+    ),
+    new ProductDTO(
+        6,
+        '6.png',
+        'Product 6',
+        'Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        82.70,
+    ),
+    new ProductDTO(
+        7,
+        '7.png',
+        'Product 7',
+        'Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        102.00,
+    ),
+    new ProductDTO(
+        8,
+        '8.png',
+        'Product 8',
+        'Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        112.50,
+    ),
+];
